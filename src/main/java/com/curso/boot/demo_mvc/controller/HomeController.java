b000d64328976f13ce0884482148497c95054102
EGACCAS <t-379345@senacsp.edu.br>
2025-08-19 22:13:09 -0300

    Refector pom ADD controller Departamento

--- src/main/java/com/curso/boot/demo_mvc/controller/HomeController.java
+++ src/main/java/com/curso/boot/demo_mvc/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.curso.boot.demo_mvc.web.controller;
+package com.curso.boot.demo_mvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
